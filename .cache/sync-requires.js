e8f45ce4ea29e48f5724066cf617519200e6f7a8
edmondniyubushobozi <niyubushobozi.edmond@outlook.com>
2022-12-31 14:51:56 -0600

    first commit

---
+++ .cache/sync-requires.js
@@ -0,0 +1,13 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/edmondniyubushobozi/Developer/Project files/20. useEffect and Deploy to Netlify/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/edmondniyubushobozi/Developer/Project files/20. useEffect and Deploy to Netlify/src/pages/404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/edmondniyubushobozi/Developer/Project files/20. useEffect and Deploy to Netlify/src/pages/index.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/edmondniyubushobozi/Developer/Project files/20. useEffect and Deploy to Netlify/src/pages/page-2.js")))
+}
+
